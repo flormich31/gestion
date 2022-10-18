09a90bfa5bdd8fe8d033b70619a54ffde617650e
flormich31 <floremicheloud@hotmail.com>
2022-10-18 07:57:28 -0300

    password database

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ var mysql = require("mysql");
 var dbConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "f22m",
+  password: "root123",
   database: "sistema",
 });
 
