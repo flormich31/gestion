0ef90d02b892aed7b47f8d54016c78c632a507f5
flormich31 <floremicheloud@hotmail.com>
2022-10-31 09:14:48 -0300

    Get groups in cards

--- client/src/pages/groups/index.js
+++ client/src/pages/groups/index.js
@@ -22,32 +22,20 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 
 const mdTheme = createTheme();
 
-function DashboardContent() {
-  const history = useHistory();
+class DashboardContent extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleGroupASelect = (event) => {
-    history.push("/groups-A");
-  };
-  const handlePressNew = (event) => {
-    history.push("/group-create");
-  };
-
-  const handleClick5 = (event) => {
-    alert("click en ABRIR grupo 5");
-  };
-  const handleClick6 = (event) => {
-    alert("click en ABRIR grupo 6");
-  };
-
-  const handleClickCod = (event) => {
-    alert("Código: 2022");
-  };
-
-  React.useEffect(() => {
+    this.state = {
+      groups: [],
+    };
+  }
+  componentDidMount() {
+    let _this = this;
     var config = {
       method: "get",
       url: "http://localhost:9000/groups",
@@ -56,161 +44,94 @@ function DashboardContent() {
     axios(config)
       .then(function (response) {
         console.log(JSON.stringify(response.data));
+
+        _this.setState(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }, []);
+  }
 
-  return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar />
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <AddButton onPress={handlePressNew} />
+  render() {
+    return (
+      <ThemeProvider theme={mdTheme}>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar />
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
+            }}
+          >
+            <Toolbar />
+            <AddButton />
 
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={4}>
-              <Grid item xs={12} md={12} lg={12}>
-                <FormControl variant="standard">
-                  <InputLabel htmlFor="input-with-icon-adornment">
-                    Buscar
-                  </InputLabel>
-                  <Input
-                    id="input-with-icon-adornment"
-                    startAdornment={
-                      <InputAdornment position="start">
-                        <SearchIcon />
-                      </InputAdornment>
-                    }
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} md={6} lg={4}>
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardMedia
-                    component="img"
-                    height="250"
-                    image="/assets/images/cuarto.png"
-                    alt="Cuarto image"
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      align="center"
-                    >
-                      4to - Cuarto grado
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      2 Estudiantes
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      1 Actividad
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={handleGroupASelect}>
-                      Abrir
-                    </Button>
-                    <Button size="small" onClick={handleClickCod}>
-                      Ver código
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={6} lg={4}>
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardMedia
-                    component="img"
-                    height="250"
-                    image="/assets/images/quinto.png"
-                    alt="Quinto image"
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      align="center"
-                    >
-                      5to - Quinto grado
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      3 Estudiantes
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      2 Actividades
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={handleClick5}>
-                      Abrir
-                    </Button>
-                    <Button size="small" onClick={handleClickCod}>
-                      Ver código
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={6} lg={4}>
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardMedia
-                    component="img"
-                    height="250"
-                    image="/assets/images/sexto.png"
-                    alt="Sexto image"
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      align="center"
-                    >
-                      6to - Sexto grado
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      1 Estudiante
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      1 Actividad
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={handleClick6}>
-                      Abrir
-                    </Button>
-                    <Button size="small" onClick={handleClickCod}>
-                      Ver código
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            </Grid>
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={12} lg={12}>
+                  <FormControl variant="standard">
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                      Buscar
+                    </InputLabel>
+                    <Input
+                      id="input-with-icon-adornment"
+                      startAdornment={
+                        <InputAdornment position="start">
+                          <SearchIcon />
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </Grid>
+                {this.state.groups.map((item, index) => (
+                  <Grid item xs={12} md={6} lg={4}>
+                    <Card sx={{ maxWidth: 345 }}>
+                      <CardMedia
+                        component="img"
+                        height="250"
+                        image="/assets/images/bienvenido.png"
+                        alt="Cuarto image"
+                      />
 
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          variant="h5"
+                          component="div"
+                          align="center"
+                        >
+                          {item.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Código de acceso: {item.code}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          1 Actividad
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">Abrir</Button>
+                        <Button size="small">Ver código</Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+
+              <Copyright sx={{ pt: 4 }} />
+            </Container>
+          </Box>
         </Box>
-      </Box>
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+    );
+  }
 }
 
-export default function Groups() {
-  return <DashboardContent />;
-}
+export default DashboardContent;
