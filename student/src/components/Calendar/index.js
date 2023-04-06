2011dee295df457b9176b8b6c313d2ed1cbce609
flormichel31 <floremicheloud@hotmail.com>
2023-04-06 09:09:39 -0300

    delet student

--- student/src/components/Calendar/index.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { Calendar } from "react-multi-date-picker";
-
-export default function Calendario() {
-  const [value, setValue] = useState(new Date());
-
-  return <Calendar></Calendar>;
-}
